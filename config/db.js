44578d2d5dd933330365503ef885413f73d8dc43
reyes-00 <boower-00@hotmail.com>
2023-08-04 16:20:38 -0600

    segundo commit

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ dotenv.config()
 console.log(process.env.DB_HOST);
 
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-  host: '127.0.0.1',
+  host: process.env.DB_HOST,
   port: '3306',
   dialect: 'mysql',
   define: {
